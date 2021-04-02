7659b3d1d0b3cd017da8774824976a7310a355ee
marcio sansone <msansone@gmail.com>
2021-04-02 11:08:24 +0100

    empty spring boot project

---
+++ src/main/java/br/com/msansone/apistockscontrol/ApiStockscontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.msansone.apistockscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStockscontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStockscontrolApplication.class, args);
+	}
+
+}
