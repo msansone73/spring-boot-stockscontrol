7958cf88210bb5b6dc4cc62ed775e04a380bc041
marcio sansone <msansone@gmail.com>
2021-04-24 19:38:24 +0100

    Added crypt password

--- src/main/java/br/com/msansone/apistockscontrol/ApiStockscontrolApplication.java
+++ src/main/java/br/com/msansone/apistockscontrol/ApiStockscontrolApplication.java
@@ -1,8 +1,10 @@
 package br.com.msansone.apistockscontrol;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class ApiStockscontrolApplication {
 
